acffd8c20f63b1e7adffeb1417a4b0603f6c60e7
tdargent1 <tdargent1@gmail.com>
2022-06-08 14:57:34 +0200

    website

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,14 @@ let currentPopup: any = undefined;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
-
-    // WA.room.onEnterLayer('website').subscribe(() => {
-    //     WA.nav.openTab('https://www.google.com');
-    // });
+    WA.room.onEnterLayer('website').subscribe(async () => {
+        await WA.nav.openCoWebSite('http://localhost:8081/');``
+        console.log(WA.player);
+    }) 
 
     WA.room.onEnterLayer('zone-a').subscribe(() => {
         WA.chat.sendChatMessage("zone A", "Brigitte Bardot");
-    })
+    })     
 
     WA.room.onEnterLayer('zone-b').subscribe(() => {
         WA.chat.sendChatMessage("zone B", "Steven Spielberg");
@@ -30,7 +28,6 @@ WA.onInit().then(() => {
     WA.room.onEnterLayer('zone-d').subscribe(() => {
         WA.chat.sendChatMessage("zone D", "Ton père l'enculé");
     })
-
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
