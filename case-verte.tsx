d9829c91b1e26fc3a499a9a668f89f865cd932cb
remi <rbrat@myges.fr>
2022-06-09 13:05:51 +0200

    [ADD] couleurs pour de vrai

---
+++ case-verte.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="case-verte" tilewidth="32" tileheight="32" tilecount="24" columns="6">
+ <image source="case-verte.png" width="192" height="128"/>
+</tileset>
