3ce5341d378060097abfb4ec8b93fbb2392e81ce
remi <rbrat@myges.fr>
2022-06-09 13:01:53 +0200

    Revert "[ADD] Zones de couleur"
    This reverts commit ba6bda5e45adbf2aba2502281f929dffb00a0c38.

--- case-bleue.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="case-bleue" tilewidth="32" tileheight="32" tilecount="24" columns="6">
- <image source="case-bleue.png" width="192" height="128"/>
-</tileset>
